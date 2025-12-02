b383e24f0144620d7f2557d83a5fa830b270b9b5
akazad12 <akazad12@aim.com>
2025-12-02 17:38:07 -0500

    part 2 problem 1

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
 
-// Check if all numbers are divisible by 5. Cache the result in a variable.
+//Part 1
 const divby5 = (n1%5==0) && (n2%5==0) && (n3%5==0) && (n4%5==0)
 console.log (`Numbers are divisible by 5 : ${divby5}.`)
 const firstBigLast = n1>n4
@@ -56,13 +56,19 @@ const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 console.log (`Is under 25: ${isUnder25}.`)
 
 
+// Part 2: Practical 
+//How many gallons of fuel will you need for the entire trip?
+total = 1500
+
+//gallons for the entire trip:
+//at 55mph
+gallonsAt55 = total/30
+//at 60mph
+gallonsAt60 = total/28
+//at 75mph
+gallonsAt75 = total/23
+console.log(gallonsAt55,gallonsAt60,gallonsAt75)
 
 
 
 
-// Check if the first number is larger than the last. Cache the result in a variable.
-// Accomplish the following arithmetic chain:
-// Subtract the first number from the second number.
-// Multiply the result by the third number.
-// Find the remainder of dividing the result by the fourth number.
-// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
